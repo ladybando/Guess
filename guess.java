d2b25fd8b551d92c6a88d09c1aa2b77ff3cca730
Ayana Bando <ayanaflax@yahoo.com>
2021-04-29 14:20:05 -0400

    Update guess.java
    update

--- guess.java
+++ guess.java
@@ -1,13 +1,15 @@
 package com.company;
 
+        import java.util.Scanner;
+
 public class Guess {
     public static void main(String[] argv) {
 
         while (true) {
             System.out.print("Select a command: ");
-            java.util.Scanner in = new java.util.Scanner(System.in);
+            Scanner in = new Scanner(System.in);
             String cmd = in.next();
-            System.out.print("Enter a value: ");
+            System.out.print("Enter a value or \"Q\" to quit: ");
 
             switch (cmd.toUpperCase()) {
                 case "F2C": {        // string match
@@ -22,26 +24,50 @@ public class Guess {
                     System.out.println("Fahrenheit: " + f + "  Celsius: " + c);
                     break;
                 }
+                case "P2K": {
+                    double p = in.nextDouble();
+                    double k = ((p / 2.2046) * .1);
+                    System.out.println("Pounds: " + p + "  Kilograms: " + k);
+                    break;
+                }
                 case "K2P": {
                     double k = in.nextDouble();
                     double p = (k * 2.20462);
                     System.out.println("Kilograms: " + k + "  Pounds: " + p);
                     break;
                 }
-                case "P2K":
-                    double p = in.nextDouble();
-                    double k = (p - 32) * 5 / 9;
-                    System.out.println("Pounds: " + p + "  Kilograms: " + k);
-
+                case "T2C": {
+                    double t = in.nextDouble();
+                    double c = (t * 0.0625);
+                    System.out.println("Tablespoons: " + t + "  Cups: " + c);
+                    break;
+                }
+                case "C2T": {
+                    double c = in.nextDouble();
+                    double t = (c * 16);
+                    System.out.println("Cups: " + t + "  Tablespoons: " + c);
+                    break;
+                }
+                case "D2E": {
+                    double d = in.nextDouble();
+                    double e = (d * 0.83);
+                    System.out.println("Dollars: $" + d + "  Euros: €" + e);
+                    break;
+                }
+                case "E2D": {
+                    double e = in.nextDouble();
+                    double d = (e * 1.20);
+                    System.out.println("Euros: €" + e + "  Dollars: $" + d);
+                    break;
+                }
                 case "Q": {
-                    System.out.println("Come convert again!");
+                    System.out.println("\n\"Q\" selected. Conversion complete. ");
                     System.exit(0);
                 }
 
                 default:
                     System.out.println("Unexpected value. Please try again: " + cmd);
             }
-
         }
     }
 }
